4594d75db4304446b223a89eab4534749eca28f2
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2019-02-27 22:51:01 -0600

    create dropdown checkbox selector

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
 console.log('im here');
+
+$('#select-all').click(function () {
+  const checkboxes = $('input[type="checkbox"]');
+  $.map( checkboxes, function( check ) {
+    check.checked = true;
+  });
+})
+
+$('#unselect-all').click(function () {
+  const checkboxes = $('input[type="checkbox"]');
+  $.map(checkboxes, function (check) {
+    check.checked = false;
+  })
+})
+
+
+
+
+
+
+
+
+// const selectAll = document.getElementById('select-all');
+
+// selectAll.addEventListener('click', function () {
+//   const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+//   [...checkboxes].map(check => check.checked = true);
+// })
+
+// const unselectAll = document.getElementById('unselect-all');
+
+// unselectAll.addEventListener('click', function() {
+//   const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+//   [...checkboxes].map(check => check.checked = false);
+// })
+
+
