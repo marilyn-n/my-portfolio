d39fa5dc5494fc0b413c906a433287f17c803e66
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2019-03-03 23:11:17 -0600

    create slider

--- main.js
+++ main.js
@@ -35,7 +35,32 @@ $('.gallery--close').click(function () {
   $('.gallery--gallery-bg').addClass('d-none');
 })
 
+var slideIndex = 1;
+showSlides(slideIndex);
 
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
 
 // const selectAll = document.getElementById('select-all');
 
