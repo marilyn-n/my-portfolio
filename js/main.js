3644a1c09d2dcf88a75b45c3517ffaf7560e5ba4
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2019-07-21 21:22:44 -0400

    fix navbar and styles

--- js/main.js
+++ js/main.js
@@ -1,59 +1,17 @@
-$('#select-all').click(function () {
-  const checkboxes = $('input[type="checkbox"]');
-  $.map( checkboxes, function( check ) {
-    check.checked = true;
-  });
+// selectors
+const menu = document.querySelector('.menu');
+const sticky = menu.offsetTop;
 
-  $('#drop-options .dropdown-icon').removeClass('fas fa-check text-blue');
-  $(this).children('.dropdown-icon').addClass('fas fa-check text-blue');
-});
+// functions
+const stickyNavbar = () => {
+    if (window.pageYOffset >= sticky) {
+        menu.classList.add('sticky');
+    }
 
-$('#unselect-all').click(function () {
-  const checkboxes = $('input[type="checkbox"]');
-  $.map(checkboxes, function (check) {
-    check.checked = false;
-  });
+    if (window.pageYOffset === 0) {
+        menu.classList.remove('sticky');
+    }
+};
 
-  $('#drop-options .dropdown-icon').removeClass('fas fa-check text-blue');
-  $(this).children('.dropdown-icon').addClass('fas fa-check text-blue');
-
-});
-
-$('#step-1').click(function () {
-  $('#email').removeClass('d-none').find('.form-control').focus();
-  $(this).attr('disabled', true);
-})
-
-$('#step-2').click(function () {
-   $('#url').removeClass('d-none').find('.form-control').focus();
-   $(this).attr('disabled', true);
-})
-
-$('#step-3').click(function () {
-  $('#message').removeClass('d-none').find('.form-control').focus();
-  $(this).attr('disabled', true);
-})
-
-$('.openbtn').mouseenter(function () {
-  $('#mySidebar').css("width", "250px");
-  $('.black-screen').removeClass('d-none');
-  $('.fa-bars').addClass('d-none');
-  // $('#main').css("marginLeft", "250px");
-})
-
-$('#mySidebar').mouseleave(function () {
-  $('#mySidebar').css("width", "48");
-  $('.fa-bars').removeClass('d-none');
-  // $('#main').css("marginLeft", "0");
-  $('.black-screen').addClass('d-none');
-})
-
-// $('#dropdownMenuSkills').mouseenter(function() {
-//   $('.dropdown-menu').addClass('show');
-// })
-
-// $('#dropdownMenuSkills').mouseleave(function() {
-//   $('.dropdown-menu').removeClass('show');
-// })
-
-
+// hook up events
+window.addEventListener('scroll', stickyNavbar)
