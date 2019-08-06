a6fea6d58d4936db1cafaad3d9b0db0cc3c9248f
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2019-08-05 21:02:13 -0400

    refactor main.js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ const nav = document.querySelector('.menu');
 const contactMenu = document.querySelector('.get-in-contact');
 const projectList = document.querySelector('.projects__list');
 const slideShow = document.querySelector('.projects__carrousel');
+const slides = document.getElementsByClassName('mySlides');
+const dots = document.getElementsByClassName('dot');
+const jobList = document.querySelector('.experience__jobs-list');
+const jobDates = document.querySelectorAll('[data-date]');
 const topOfNav = 122;
 
 // functions
@@ -27,48 +31,31 @@ const reportWindowSize = () => {
 };
 
 const sortByDate = () => {
-    const jobList = document.querySelector('.experience__jobs-list');
-    const jobDates = document.querySelectorAll('[data-date]');
-
     const newJobItems = [...jobDates]
         .sort((a, b) => new Date(b.dataset.date) - new Date(a.dataset.date))
         .forEach(job => {
             jobList.append(job)
 
         });
+};
 
-}
+let slideIndex = 1;
+const showSlides = (n) => {
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+
+    [...slides].map(slide => slide.style.display = 'none');
+    [...dots].map(dot => dot.className = dot.className.replace(' active', ''));
+
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].className += ' active';
+};
+
+const currentSlide = (n) => showSlides(slideIndex = n);
 
 // hook up events
-window.addEventListener('scroll', fixNav)
+window.addEventListener('scroll', fixNav);
 window.addEventListener('resize', reportWindowSize);
 window.onload = reportWindowSize();
 window.onload = sortByDate();
-
-
-// slideshow--------------------------------------------------------------------------------------
-
-const showSlides = (n) => {
-    let i;
-    const slides = document.getElementsByClassName("mySlides");
-    const dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-const currentSlide = (n) => {
-    showSlides(slideIndex = n);
-}
-
-// slideshow--------------------------------------------------------------------------------------,
+window.onload = showSlides(slideIndex);
