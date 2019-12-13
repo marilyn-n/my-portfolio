814dd42a6d689a2d418829ea96c8a2a663b36355
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2019-12-12 19:10:53 -0500

    add minor changes

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ const fixNav = () => {
 };
 
 const reportWindowSize = () => {
-    if (window.innerWidth <= 768) {
+    if (window.innerWidth <= 425) {
         projectList.classList.add('d-none');
         slideShow.classList.remove('d-none');
     } else {
