99c9f678077642887143df82f2fc9ebdc4311511
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2020-05-29 18:05:19 -0400

    add new styles and animations to about and project sections

--- js/main.js
+++ js/main.js
@@ -1,23 +1,15 @@
 // selectors
-const nav = document.querySelector('.menu');
-const contactMenu = document.querySelector('.get-in-contact');
 const projectList = document.querySelector('.projects__list');
 const slideShow = document.querySelector('.projects__carrousel');
 const slides = document.getElementsByClassName('mySlides');
 const dots = document.getElementsByClassName('dot');
 const jobList = document.querySelector('.experience__jobs-list');
 const jobDates = document.querySelectorAll('[data-date]');
-const copyBtn = document.querySelector('.about-me__copy-to-clipboard__gmail--copy');
-const switchBtn = document.querySelector('.switch-btn input[type="checkbox"]');
-const html = document.querySelector('html');
-const alert = document.querySelector('.tip-wrapper__tip');
+const switchBtn = document.querySelector('label.switch-btn input[type="checkbox"]');
+const html = document.querySelector('HTML');
 const cardHeaders = document.querySelectorAll('.a-card--wrapper');
 
-const topOfNav = 122;
-
 // functions
-const fixNav = () => window.scrollY >= topOfNav ? nav.classList.remove('d-none') : nav.classList.add('d-none');
-
 const reportWindowSize = () => {
     if (window.innerWidth <= 500) {
         projectList.classList.add('d-none');
@@ -50,22 +42,8 @@ const showSlides = (n) => {
 
 const currentSlide = (n) => showSlides(slideIndex = n);
 
-const copyToClipboard = () => {
-    const input = document.querySelector('input.email-to-clipboard');
-    input.select();
-    document.execCommand("copy");
-};
-
 function handleDarkMode() {
     this.checked ? html.classList.add('dark-mode') : html.classList.remove('dark-mode');
-};
-
-const showAlert = () => {
-    alert.style.opacity = 1;
-    alert.style.transition = 'opacity .25s ease-in-out';
-    setTimeout(() => { 
-        alert.style.opacity = 0;
-    }, 2500);
 };
 
 function modal() {
@@ -117,12 +95,9 @@ function modal() {
 };
 
 // hook up events
-window.addEventListener('scroll', fixNav);
 window.addEventListener('resize', reportWindowSize);
 window.onload = reportWindowSize();
 window.onload = sortByDate();
 window.onload = showSlides(slideIndex);
-copyBtn.addEventListener('click', copyToClipboard);
-copyBtn.addEventListener('click', showAlert);
 switchBtn.addEventListener('change', handleDarkMode);
 [...cardHeaders].map(item => item.addEventListener('click', modal));
